430e6ef68583a64da8d18382cdf209295d7110e5
Nadezhda-Belonogova <nadezhda-belonogova@yandex.ru>
2022-03-17 08:44:37 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=lpgvie1juq+6*3&1i(woke!mcu-=%06d0!9=h7)gqc&ds0vis'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nadyabel.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
